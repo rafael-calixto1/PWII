f5a1c14a48c3e46523e1b4bd03bfd0d9699f1f12
rafael-calixto1 <77207863+rafael-calixto1@users.noreply.github.com>
2022-04-30 15:21:32 -0300

    exercises and documentation of what I learn in PHP

---
+++ aulaPHP/ex001.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+            echo "integer <br><br>";
+            $n1 = 10;
+            $n2 = 30;
+            $n3 = 120;
+            
+            echo $n1. "<br>";
+            echo $n2. "<br>";
+            echo $n3. "<br><br>";
+            
+            echo "floats<br>";
+            $n11 = 10.01;
+            $n21 = 30.001;
+            $n31 = 120.001;
+            
+
+            echo $n11. "<br>";
+            echo $n21. "<br>";
+            echo $n31. "<br>";
+            
+
+            echo"<br><br>";
+
+            echo"Concatenando a variavel Idade e a varivael com texto<br><br>";
+            $idade = 15; $preco = 12.00;
+            echo"Ele tem $idade e pagou R$$preco,00 em um lanche<br><br>";
+
+
+            //ARRAYS
+            $array = [1,2,3];
+            print_r($array); //apenas para arrays
+
+            echo "<br><br>".$array[2];
+
+            $nome = ["Marcos", 1005, true];
+
+            echo "<br>".$nome[1];
+        ?>
+</body>
+</html>
+
---
+++ aulaPHP/exercicios.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        echo "PHP is a language for the backEnd on Web, more than 70% of web sites uses this programming language<br>";
+        echo"PHP is case sensitive<br>";
+        echo"you can do a comment using the '//' for single line comments and '/*  */ for multiple lines comments'<br>";
+        echo"To declare variables you need to use the '$' signal in the begin and rather than others languages, is good pratice to use the '_' underScore to use  instead of use nothing less than a simple upperCase letter <br>";
+        echo"PHP is losely typed<br>";
+        echo"data types in PHP: <br>
+            String<br>
+            Integer<br>
+            Float (floating point numbers - also called double)<br>
+            Boolean<br>
+            Array<br>
+            Object<br>
+            NULL<br>
+            Resource <br>";
+        echo"Constants in PHP are within a function define(name, value, case-sensitive)<br>";
+        echo"The opperators in PHP are: +, -, *, /, %, **, see more in W3 Schools blog";
+        echo"EXAMPLE OF PHP IF ELSE STATEMENTS";
+        
+        
+    ?>
+
+</body>
+</html>
